ea628a0798b16890566fdb44140b0d1c9d9db0fc
baekjiyeon <whitedelay@gmail.com>
2019-10-15 20:11:06 +0900

    Assignment 2 완료

--- AI_edge_detection.py
+++ AI_edge_detection.py
@@ -1,15 +1,17 @@
 import cv2
 import numpy as np
 import AI_function as func
-
+import math
 
 
 # 2-1 Apply the Gaussian filtering to the input image
 
 
 # 2-2 Implement a function that returns the image gradient
-func.compute_image_gradient("lenna.png")
-func.compute_image_gradient("shapes.png")
-cv2.waitKey()
+mag,dir = func.compute_image_gradient("lenna.png")
+func.non_maximum_suppression_dir(mag,dir)
+# func.compute_image_gradient("shapes.png")
+# cv2.waitKey()
 
 # 2-3 Implement a function that performs Non-maximum Suppression (NMS)
+
--- AI_function.py
+++ AI_function.py
@@ -3,6 +3,7 @@ import numpy as np
 import math
 import time
 
+
 #### For Assignment 1 ####
 
 def image_padding(img, V, H):
@@ -61,7 +62,6 @@ def cross_correlation_2d(img, kernel):
     output = np.zeros(img.shape)
     v_pad_size = int(kernel.shape[0] / 2)
     h_pad_size = int(kernel.shape[1] / 2)
-
     img = image_padding(img, v_pad_size, h_pad_size)
 
     kernel_idx = [(i, j) for i in range(kernel.shape[0]) for j in range(kernel.shape[1])]
@@ -70,7 +70,6 @@ def cross_correlation_2d(img, kernel):
             for (dy, dx) in kernel_idx:
                 output[y, x] += kernel[dy, dx] * img[y + dy, x + dx]
             output[y, x] /= 255.0
-
     return output
 
 
@@ -103,13 +102,13 @@ def get_gaussian_filter_2d(size, sigma):
 
 #### For Assignment 2 ####
 
-def sobel_filtering(image,axis):
+def sobel_filtering(image, axis):
     blurring = np.array([1, 2, 1])
-    derivative_1D = np.array([1, 0, -1])
-    if(axis==0):
-        return cross_correlation_1d(255*cross_correlation_1d(image,blurring.reshape(3, 1)),derivative_1D)
+    derivative_1D = np.array([-1, 0, 1])
+    if (axis == 0):
+        return cross_correlation_1d(255 * cross_correlation_1d(image, blurring.reshape(3, 1)), derivative_1D)
     else:
-        return cross_correlation_1d(255*cross_correlation_1d(image,derivative_1D.reshape(3, 1)),blurring)
+        return cross_correlation_1d(255 * cross_correlation_1d(image, derivative_1D.reshape(3, 1)), blurring)
 
 
 def compute_image_gradient(imagepath):
@@ -117,37 +116,57 @@ def compute_image_gradient(imagepath):
     start = time.time()
 
     image = cv2.imread(imagepath, cv2.IMREAD_GRAYSCALE)
+
     # 2-1 Apply the Gaussian filtering to the input image
     image = cross_correlation_2d(image, get_gaussian_filter_2d(7, 1.5))
 
-    output_mag = np.zeros(image.shape)
-    output_dir = np.zeros(image.shape)
+    # mag, dir 값 포함하는 3차원 np array 생성 - [2][y][x]
+    output = np.zeros([2] + list(image.shape))
 
     # calculating gradient
-    xdir_grad = sobel_filtering(255*image,axis=0)
-    ydir_grad = sobel_filtering(255*image,axis=1)
+    xdir_grad = sobel_filtering(255 * image, axis=0)
+    ydir_grad = sobel_filtering(255 * image, axis=1)
 
     for y in range(image.shape[0]):
         for x in range(image.shape[1]):
-            df_dx = xdir_grad[y,x]
-            df_dy = ydir_grad[y,x]
+            df_dx = xdir_grad[y, x]
+            df_dy = ydir_grad[y, x]
+            # direction 계산
 
-            dir = math.atan2(df_dy,df_dx) * 180/math.pi
-            # print("dir",dir)
-            mag = math.sqrt((df_dx*df_dx) + (df_dy*df_dy))
+            # 1사분면을 양수로.
+            dir = math.atan2(-df_dy, df_dx) * (180 / math.pi)
+            dir = 180+dir if dir<0 else dir
+
+            # magnitude 계
+            mag = math.pow((df_dx * df_dx) + (df_dy * df_dy), 1 / 2)
             # print("mag",mag)
 
-            output_dir[y,x] = dir
-            output_mag[y,x] = mag
+            output[0][y, x] = mag
+            output[1][y, x] = dir
 
-    print("computing image gradient Time: ",time.time()-start)
+    print("computing image gradient Time: ", time.time() - start)
 
-    # image verification
-    cv2.imshow("image gradient", output_mag)
-    # saving image
-    cv2.imwrite("./result/part_2_edge_raw_" + imagepath, 255 * output_mag)
+    return output[0], output[1]
 
-    return output_mag,output_dir
 
-def non_maximum_suppression_dir(mag,dir):
+def non_maximum_suppression_dir(mag, dir):
 
+    M, N = mag.shape
+    # direction to array index pair
+    dy = [[0, 0], [-1, 1], [-1, 1], [-1, 1]]
+    dx = [[1, -1], [1, -1], [0, 0], [-1, 1]]
+    output = mag.copy()
+
+    for i in range(1, M - 1):
+        for j in range(1, N - 1):
+            # quantize
+            q = int((22.5+dir[i, j]) / 45)%4  # 0 ~ 3 values
+            # suppression
+            if (mag[i, j] <= mag[i+dy[q][0],j+dx[q][0]]) or (mag[i, j] <= mag[i+dy[q][1],j+dx[q][1]]):
+                output[i, j] = 0
+
+
+    cv2.imshow("image suppressed", output)
+    cv2.waitKey()
+
+    return
